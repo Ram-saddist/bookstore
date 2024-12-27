56302d372275e406433be7abfcfc6384bc2bde50
Ram-saddist <nagulavancharavisivaramteja@gmail.com>
2024-12-27 14:10:14 +0530

    home card stylig

--- frontend/src/Home/Home.jsx
+++ frontend/src/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import './Home.css'
 export default function Home() {
   const [books, setBooks] = useState([])
   useEffect(() => {
@@ -15,28 +16,36 @@ export default function Home() {
   return (
     <div>
       <h1>Books</h1>
-
       {
         books.length === 0 ? (
           <div>Currently we dont have any books </div>
         ) : (
-          <ul className='books-container'>
-
-            {
-              books.map((book) => {
-                return <li key={book._id}>
-                          {console.log("books details", book)}
-                          <h1>{book.title}</h1>
-                          <p>{book.author}</p>
-                          <p>{book.price}</p>
-                          <p>{book.description}</p>
-                          <img src={book.imageUrl} alt={book.title} width="30%" />
-                        </li>
-              })
-            }
-          </ul>
+          <div className='container'>
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+              {
+                books.map((book) => {
+                  return <div className='col' key={book._id}>
+                          <div className="card h-100">
+                            <img className="card-img-top" src={book.imageUrl} alt={book.title} />
+                            <div className="card-body">
+                              <h1 className="card-title">{book.title}</h1>
+                              <p className="card-text">{book.author}</p>
+                              <p className="card-text">{book.price}</p>
+                              <p className="card-text">{book.description}</p>
+                             
+                            </div>
+                            <div class="card-footer">
+                              <button className="btn btn-warning  fw-bold">Edit</button>
+                              </div>
+                          </div>
+                          
+                        </div>
+                })
+              }
+            </div>
+          </div>
         )
       }
-    </div>
+    </div >
   )
 }
